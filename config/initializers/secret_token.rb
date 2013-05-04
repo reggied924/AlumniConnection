d068ad955269244e794324fe41bdb46d9fbcabd1
Reginald Daniel <redaniel@valdosta.edu>
2013-05-03 20:02:00 -0400

    Started project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlumniConnection4::Application.config.secret_token = 'a79157e2a704002da4fe7d5e672c615b9bd95cfe98586b5ecc8fa14db8914e92e4cada6602fd481db3b848d703b822a06cd1a9b1ea3efe676cab7f27f9648dc0'
